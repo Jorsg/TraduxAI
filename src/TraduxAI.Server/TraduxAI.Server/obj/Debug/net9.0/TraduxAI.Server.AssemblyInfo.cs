4c9ac29931839c3187a7b1ca553c1174b1a1a6b4
Jorman Garcia <jagarcia@lfs-inc.com>
2025-04-04 11:30:41 -0400

    add file gitignore

--- src/TraduxAI.Server/TraduxAI.Server/obj/Debug/net9.0/TraduxAI.Server.AssemblyInfo.cs
+++ src/TraduxAI.Server/TraduxAI.Server/obj/Debug/net9.0/TraduxAI.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TraduxAI.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cabcc6564e6aa50fc603faab3179a8a2ff072ca1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7751c2ad1358dcd9b96ce6367d94235efde9408")]
 [assembly: System.Reflection.AssemblyProductAttribute("TraduxAI.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TraduxAI.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
